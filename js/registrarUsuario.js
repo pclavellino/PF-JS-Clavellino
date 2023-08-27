7028dfc3ed5eca446d9e61b5c65a9a6f4a1c0623
Pablo Clavellino <pclavellino@hotmail.com>
2023-08-27 15:01:17 -0300

    Uso de API FormSubmit en formulario de pagina contacto.

--- js/registrarUsuario.js
+++ js/registrarUsuario.js
@@ -1,48 +1,33 @@
-fetch("../json/baseDatosUsuarios.json")
-    .then ((respuesta) => respuesta.json())
-    .then ((usuariosRegistrados) => {
-    
-        function Usuario(nombre, mail, contraseña) {
-            this.nombre = nombre.value;
-            this.mail = mail.value;
-            this.contraseña = contraseña.value;
-        }
-        
-        function registrarUsuario() {
-        
-            const nombre = document.getElementById("regNombre");
-            const mail = document.getElementById("regMail");
-            const contraseña = document.getElementById("regContraseña");
-        
-            if (nombre.value == "" || mail.value == "" || contraseña.value == "") {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Todos los campos son necesarios. Revise los datos e intente nuevamente',
-                })
-            } else {
-                const nuevoUsuario = new Usuario(nombre, mail, contraseña);
-                fetch("../json/baseDatosUsuarios", {
-                    method: "POST",
-                    body: JSON.stringify(nuevoUsuario)
-                })
-                .then((respuesta) => respuesta.json())
-                .then((datos) => console.log(datos))
-                /*usuariosRegistrados.push(nuevoUsuario);*/
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Usuario registrado con exito',
-                })  
-            }
-        }
-        
-        document.getElementById("registrarse").addEventListener("click", registrarUsuario);
+import usuariosRegistrados from "./baseDatosUsuarios.js";
 
-    })
+function Usuario(nombre, mail, contraseña) {
+    this.nombre = nombre.value;
+    this.mail = mail.value;
+    this.contraseña = contraseña.value;
+}
 
+function registrarUsuario() {
 
-/*import usuariosRegistrados from "./baseDatosUsuarios.js";*/
+    const nombre = document.getElementById("regNombre");
+    const mail = document.getElementById("regMail");
+    const contraseña = document.getElementById("regContraseña");
 
+    if (nombre.value == "" || mail.value == "" || contraseña.value == "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Todos los campos son necesarios. Revise los datos e intente nuevamente',
+        })
+    } else {
+        const nuevoUsuario = new Usuario(nombre, mail, contraseña);
+        usuariosRegistrados.push(nuevoUsuario);
+        Swal.fire({
+            icon: 'success',
+            title: 'Usuario registrado con exito',
+        })  
+    }
+}
 
+document.getElementById("registrarse").addEventListener("click", registrarUsuario);
 
 
